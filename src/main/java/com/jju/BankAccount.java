0c0f6ccdcb4dee504d06fed7e35ac3ede3ae1a4f
surafelwegayehu516-cell <surafelwegayehu516@gmail.com>
2026-04-03 17:11:45 +0300

    Compleated BankAccount Assignment

--- src/main/java/com/jju/BankAccount.java
+++ src/main/java/com/jju/BankAccount.java
@@ -1,3 +1,4 @@
+package com.jju;
 public class BankAccount {
     private String accountHolder;
     protected double balance; // Protected so subclasses can see it, but not the public
@@ -5,16 +6,26 @@ public class BankAccount {
     public BankAccount(String accountHolder, double initialBalance) {
         this.accountHolder = accountHolder;
         // Student Task: Ensure balance cannot be negative during initialization
+         this.balance = (initialBalance < 0) ? 0.0 : initialBalance;
     }
 
     public void deposit(double amount) {
         // Student Task: Implement deposit logic with validation
+          if (amount > 0) {
+            balance += amount;
+        }
     }
 
     public void withdraw(double amount) {
         // Student Task: Implement withdrawal logic (prevent overdraft)
+           if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        }
     }
 
+    public String getAccountHolder() {
+    return accountHolder;
+}
     public double getBalance() {
         return balance;
     }
--- src/main/java/com/jju/SavingsAccount.java
+++ src/main/java/com/jju/SavingsAccount.java
@@ -5,14 +5,20 @@ public class SavingsAccount extends BankAccount {
 
   // 1. Student Task: Create constructor here 
     
+ public SavingsAccount(String accountHolder, double initialBalance, double interestRate) {
+        super(accountHolder, initialBalance);
+        this.interestRate = interestRate;
+    }
 
     public void applyInterest() {
         // 2. Student Task: Implement interest application logic here
-        
+          double interest = balance * interestRate;
+        deposit(interest);
     }
 
   // 3. Student Task: Create getter for interestRate here
     
-    
+    public double getInterestRate() {
+    return interestRate;
 }
-
+}
